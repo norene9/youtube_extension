3e3b6ab8b3c84e471bf6ed2d1ce42acf417489ab
FatyAmr <81917775+FatyAmr@users.noreply.github.com>
2021-08-20 02:11:57 +0200

    Delete Extension directory

--- Extension/loginInit.js
+++
@@ -1,9 +0,0 @@
-function init() {
-    chrome.runtime.sendMessage({ message: 'is_user_signed_in' }, function (response) {
-        if (response.message === 'success' && response.payload) {
-            window.location.replace('./child.html');
-        }
-    });
-}
-
-init();
